b2bac528898f44f8f744c5115f1c7260a23eec3d
asihizkiahu <asih@liveperson.com>
2015-12-30 15:32:53 +0200

    add missing files

--- src/main/java/com/test/BaseTest.java
+++ src/main/java/com/test/BaseTest.java
@@ -296,7 +296,7 @@ public class BaseTest {
 
     protected static class Logging {
 
-        private void configureLog4J() {
+        protected void configureLog4J() {
             java.net.URL url = getClass().getResource(
                     getProps().getProperty(
                             LOG4J_PROP_FILE_PATH_KEY_VALUE)
